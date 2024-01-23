29d3811b16b1f41a546d0c803f2aafbf8b612f1d
Arun-vignesh <arunvignesh0707@gmail.com>
2024-01-23 09:47:25 +0530

    Initial setup

---
+++ DataStructures/SimpleLinkedList/SimpleLinkedList.js
@@ -0,0 +1,4 @@
+import node from './node.js'
+
+const newNode = new node('arun', 'vignesh')
+console.log('newNode>>>', newNode);
---
+++ DataStructures/SimpleLinkedList/node.js
@@ -0,0 +1,6 @@
+export default class node {
+    constructor(val, nextVal){
+        this.val = val;
+        this.next = nextVal
+    }
+}
